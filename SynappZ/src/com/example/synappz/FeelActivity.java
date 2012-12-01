dd248b94e300020a5cb129b2ab89409903b97681
007 <007@007-PC>
2012-12-01 02:01:27 +0200

    first commit

---
+++ SynappZ/src/com/example/synappz/FeelActivity.java
@@ -0,0 +1,15 @@
+package com.example.synappz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeelActivity extends Activity {
+	
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.cactivity_feelscreen);
+		}
+
+}
